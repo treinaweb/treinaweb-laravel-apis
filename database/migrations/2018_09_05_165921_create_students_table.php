4a5815523fe7f2d0fe5a888bc52432b341ee0bd5
Elton Fonseca <elton869@gmail.com>
2018-09-05 14:07:44 -0300

    3 - criando as tabelas no banco de dados

---
+++ database/migrations/2018_09_05_165921_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->date('birth');
+            $table->char('gerder', 1);
+
+            $table->unsignedInteger('classroom_id');
+            $table->foreign('classroom_id')->references('id')->on('classrooms');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}

64be81ac0b050a59206211dfea71374e07d139de
Elton Fonseca <elton869@gmail.com>
2018-09-21 11:07:09 -0300

    28 - camada de transformação

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -13,33 +13,33 @@ class Student extends Model
      */
     protected $fillable = ['name', 'birth', 'gerder', 'classroom_id'];
 
-    /**
-     * Faz conversão na hora de serialização
-     *
-     * @var array
-     */
-    protected $casts = [
-        'birth' => 'date:d/m/Y'
-    ];
+    // /**
+    //  * Faz conversão na hora de serialização
+    //  *
+    //  * @var array
+    //  */
+    // protected $casts = [
+    //     'birth' => 'date:d/m/Y'
+    // ];
 
-    /**
-     * Define atributos não mostrados depois da serialização
-     */
-    //protected $hidden = ['created_at', 'updated_at'];
+    // /**
+    //  * Define atributos não mostrados depois da serialização
+    //  */
+    // //protected $hidden = ['created_at', 'updated_at'];
 
-    /**
-     * Define os atributos visiveis depois da serialização
-     *
-     * @var array
-     */
-    protected $visible = ['name', 'gerder', 'birth', 'classroom_id', 'is_accepted'];
+    // /**
+    //  * Define os atributos visiveis depois da serialização
+    //  *
+    //  * @var array
+    //  */
+    // protected $visible = ['name', 'gerder', 'birth', 'classroom_id', 'is_accepted'];
 
-    /**
-     * Define os atributos dinamicos anexos a serialização
-     *
-     * @var array
-     */
-    protected $appends = ['is_accepted'];
+    // /**
+    //  * Define os atributos dinamicos anexos a serialização
+    //  *
+    //  * @var array
+    //  */
+    // protected $appends = ['is_accepted'];
 
     /**
      * mapeamento do relacionamento com salas de aula
@@ -51,13 +51,13 @@ class Student extends Model
         return $this->belongsTo('App\Models\Classroom');
     }
 
-    /**
-     * cria um assessor no model de studante chamado is_accepted
-     *
-     * @return void
-     */
-    public function getIsAcceptedAttribute()
-    {
-        return $this->attributes['birth'] > '2002-01-01' ? 'aceito' : 'não foi aceito';
-    }
+    // /**
+    //  * cria um assessor no model de studante chamado is_accepted
+    //  *
+    //  * @return void
+    //  */
+    // public function getIsAcceptedAttribute()
+    // {
+    //     return $this->attributes['birth'] > '2002-01-01' ? 'aceito' : 'não foi aceito';
+    // }
 }

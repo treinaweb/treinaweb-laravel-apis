40d3ad4605295501eb2c62bb54b403e7993a2fb5
Elton Fonseca <elton869@gmail.com>
2018-09-26 17:34:21 -0300

    44 - Excluindo estudantes(app cliente)

--- public/crud.js
+++ public/crud.js
@@ -1,6 +1,15 @@
 var baseUrl = "http://escola.test/api";
 
 $(document).ready(function() {
+  list();
+
+  $('#list-body').on('click', '.delete-button', function(){
+    excluir($(this).attr("data-id"))
+  })
+
+});
+
+function list() {
   $.ajax({
     type: "GET",
     url: `${baseUrl}/students`,
@@ -15,12 +24,27 @@ $(document).ready(function() {
           <td>${student.name}</td>
           <td>${student.birth}</td>
           <td>${student.gender}</td>
+          <td>
+            <button class="delete-button" data-id="${student.id}">Excluir</button>
+          </td>
         </tr>
         `
       }
 
       $('#list-body').html(content);
-
     }
   });
-});
+}
+
+function excluir(id) {
+  $.ajax({
+    type: "DELETE",
+    url: `${baseUrl}/students/${id}`,
+    contentType: "application/json",
+    success: function(students) {
+      alert('O aluno foi excluido com sucesso!');
+
+      list();
+    }
+  });
+}
